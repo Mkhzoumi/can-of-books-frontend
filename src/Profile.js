291c892491aa5ae8f77ca764ea72f16591b921dd
mkhzoumi <zx.hammam@hotmail.com>
2021-06-20 17:34:58 +0300

    header , header logout button , solve env prop

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withAuth0 } from "@auth0/auth0-react";
+import Card from 'react-bootstrap/Card';
 
 export class Profile extends Component {
     constructor(props) {
@@ -13,11 +14,25 @@ export class Profile extends Component {
     render() {
         // console.log(this.props.auth0);
         return (
-            <div>
-                <h2>{this.state.userName}</h2>
-                <p>{this.state.userEmail}</p>
-                <img src={this.state.userPicture} alt={this.state.userName} />
-            </div>
+
+            <Card style={{ width: '18rem' , 'margin-top':'100px', 'margin-bottom':'100px'}}>
+  <Card.Img variant="top" src={this.state.userPicture} />
+  <Card.Body>
+    <Card.Title>{this.state.userName}</Card.Title>
+    <Card.Text>
+    {this.state.userEmail}
+    </Card.Text>
+  </Card.Body>
+</Card>
+
+
+
+
+            // <div>
+            //     <h2>{this.state.userName}</h2>
+            //     <p>{this.state.userEmail}</p>
+            //     <img src={this.state.userPicture} alt={this.state.userName} />
+            // </div>
         )
     }
 }
